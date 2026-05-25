9f3fcce8f1845164992fe4ecb5a5bfec491c73bb
ltdrobot <ltdrobot@localizethedocs.org>
2026-05-25 13:10:19 +0000

    Deploying to pages from @ localizethedocs/rtd-dev-docs-l10n@0ecf7fbd9deb0c84f44fd30ea66d8c9435cdb30f 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/rtd-dev-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/rtd-dev-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/rtd-dev-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/rtd-dev-docs-l10n"],
+  ]
+};
